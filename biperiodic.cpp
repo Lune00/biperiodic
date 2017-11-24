a4755b4c54555d3729239fe4f3c3ca9bd3937332
Paul Schuhmacher <paul.schuhmacher@laposte.net>
2017-11-24 17:44:32 +0100

    integration particule en force ok, en vitesse?

--- biperiodic.cpp
+++ biperiodic.cpp
@@ -24,7 +24,7 @@ class Vecteur{
 
 		Vecteur productTensor(Tensor2x2&);
 		void print(){cout<<x_<<" "<<y_<<endl;}
-		void update(double x,double y){ x_=x; y_=y;}
+		void set(double x,double y){ x_=x; y_=y;}
 		void add(double dx, double dy) { x_ += dx ; y_ += dy;}
 
 		//Accessors:
@@ -34,6 +34,26 @@ class Vecteur{
 		//Mutators:
 		void setx(double x) {x_ = x;}
 		void sety(double y) {y_ = y;}
+
+		//Surcharge operator:
+		//Scalar product:
+		double operator * (Vecteur a){
+			return (x_*a.x_ + y_*a.y_);
+		}
+
+		Vecteur operator + (Vecteur a){
+			Vecteur p;
+			p.x_ = x_ + a.x_;
+			p.y_ = y_ + a.y_;
+			return p;
+		}
+
+		Vecteur operator * (double a){
+			Vecteur p;
+			p.x_ = a*x_ ;
+			p.y_ = a*y_ ;
+			return p;
+		}
 };
 
 class Tensor2x2{
@@ -98,6 +118,14 @@ class Tensor2x2{
 			p.xy_ = xy_+a.xy_;
 			p.yx_ = yx_+a.yx_;
 			p.yy_ = yy_+a.yy_;
+			return p;
+		}
+
+		Vecteur operator * (Vecteur a){
+			Vecteur p;
+			double x = xx_*a.getx()+ xy_*a.gety();
+			double y = yx_*a.getx() + yy_*a.gety();
+			p.set(x,y);
 			return p;
 		}
 };
@@ -200,26 +228,57 @@ class Particle{
 		Vecteur s_;
 		//Vitesse:
 		Vecteur v_;
-		//Acceleration:
-		Vecteur a_;
+		//Acceleration debut pas de temps:
+		Vecteur a0_;
+		//Forces:
+		Vecteur f_;
+		//Torque:
+		double t_;
+		//Vrot:
+		double vrot_;
+		//Rot: angular position
+		double rot_;
+		//mass:
+		double m_;
+		//Rayon:
+		double R_;
+		
 	public:
-		Particle(): r_(), v_() {}; 
-		Particle(Vecteur r, double L, Vecteur v) {r_ = r; v_ = v; calculReduced(L);}
+		Particle(): r_(), v_(), s_(), a0_(), f_(), t_(0.), vrot_(0.), rot_(0.), m_(1.), R_(1.) {}; 
+		Particle(Vecteur r, double L, Vecteur v) {r_ = r; v_ = v; init(L);}
 		~Particle(){};
+
 		void updateSimple(double dt);
-		Vecteur& getR() { return r_;}
 		void write(ofstream&);
-		void calculReduced(double L);
+		void init(double L);
 		void affiche();
+
+		double getMasse() const {return m_;}
+		double getRadius() const { return R_;}
+		double getTorque() const {return t_;}
+
+		Vecteur getR() const { return r_;}
+		Vecteur getV() const { return v_;}
+		Vecteur getForce() const { return f_;}
+		Vecteur geta0() const {return a0_;}
+		void setV(Vecteur v) { v_=v;}
+		void setR(Vecteur r, Vecteur a0) { r_ = r; a0_=a0;}
+		
 };
 
 //sx et sy doivent etre compris entre 0 et 1 (initialiement dans la cellule)
-void Particle::calculReduced(double L){
-	s_.update(r_.getx() / L,r_.gety() / L);
+void Particle::init(double L){
+	//Les vecteurs appellent constructeur, tout egal a 0
+	m_=1.;
+	R_=0.1;
+	vrot_=0.;
+	rot_=0.;
+	t_=0.;
+	s_.set(r_.getx() / L,r_.gety() / L);
 }
 
 void Particle::write(ofstream& of){
-	of<< r_.getx()<<" "<<r_.gety()<<endl;
+	of<< r_.getx()<<" "<<r_.gety()<<" "<<v_.getx()<<" "<<v_.gety()<<endl;
 }
 
 void Particle::affiche(){
@@ -233,7 +292,7 @@ void Particle::affiche(){
 void Particle::updateSimple(double dt){
 	double x = r_.getx() + v_.getx() * dt;
 	double y = r_.gety() + v_.gety() * dt;
-	r_.update(x,y);
+	r_.set(x,y);
 }
 
 //Permet de definir l'essai mecanique:Conditions aux limites (force/vitesse), mais aussi les parametres cellule etc...
@@ -265,11 +324,11 @@ Config::Config(){
 	for(int i=0;i<4;i++){
 		BCU[i] = 'v';
 	}
-	//BCU[3]='f';
+	BCU[3]='f';
 	//Par souci de convention, les DL non controles sont init a 0
 	//Par definition, le taux de cisaillement pur est egale a la moitie de LdUser.yx(ou xy)
-	LdUser_.set(0.,1.,0.,0.);
-	StressUser_.set(0.,0.,0.,0.);
+	LdUser_.set(0.,0.,0.,0.);
+	StressUser_.set(0.,0.,0.,4.);
 }
 
 //Cellule periodique
@@ -421,14 +480,12 @@ void Cell::PeriodicBoundaries(Particle& p){
 //Update h et hd de la cellule, ainsi que L et strain tensor
 //On peut meme updater Ld, car par construction le control en vitesse se fait par le controle en force
 //Normalement si on change Ld ca ne changera pas le Ld initial
-//A partir de la maj de Ld on peut facilement calculer le tenseur de deformations
+//A partir de la maj de Ld on peut facilement calculer le tenseur de deformations, oui mais ca marche pas... ????
 //UP
 void Cell::update(Tensor2x2 h, Tensor2x2 hd, double dt){
 
 	h_ = h;
 	hd_ = hd;
-
-	hd_.affiche();
 	Tensor2x2 hinv = h_.getInverse();
 
 	Ld_ = hd_*hinv;
@@ -437,7 +494,7 @@ void Cell::update(Tensor2x2 h, Tensor2x2 hd, double dt){
 	//Calcul du tenseur de deformation de maniere generale cumule
 	//s_ = s_ +  (Ld_ + LdT) * dt;
 
-	//Provisoire, test:
+	//Provisoire:
 	double Lx0= h0_.getxx();
 	double Ly0= h0_.getyy();
 	double Lxt= h_.getxx();
@@ -501,32 +558,23 @@ void Cell::ApplyBC(){
 
 //Update particules, calcul tenseur contrainte interne, update cellule
 
-//Integre le mouvement des particules avec l etat cellule au temps t
-void verletalgo_particles(Cell& cell,std::vector<Particle>& ps,double dt){
-
-	//On recupere h,hd,hdd
-	Tensor2x2 h = cell.geth();
-	Tensor2x2 hd = cell.gethd();
-	Tensor2x2 hdd = cell.gethd();
-	//Calcul de la resultante des forces entre particules
-	//Calcul du tenseur de contraintes interne
-	//Calcul acceleration pour chaque particule (force et acc de l'espace via sigma_ext)
-	//Integration par verletalgo
-
-}
-
 //On integre l'equation de la dynamique pour l'espace avec un algo de verlet
 //Integre la dynamique de l'espace, de la cellule au temps t, a besoin tenseur contraintes
-//On recupere le tenseur de contraintes internes calcule par verletalgo_particles
-void verletalgo_space(Cell& cell,std::vector<Particle>& ps, double dt){
+//Il y a couplage entre debut et fin du pas de temps
+//Il faut integrer les deux en meme temps
+void verletalgo(Cell& cell,std::vector<Particle>& ps, double dt){
 
+	//Attention: variable globale algo hinv, stress ext et int
+	//Init:
 	Tensor2x2 h = cell.geth();
 	Tensor2x2 hd = cell.gethd();
-
 	Tensor2x2 hinv = h.getInverse();
 
 	Tensor2x2 hdd0;
 	Tensor2x2 hdd1 = hdd0;
+
+	double mh=cell.getMasse();
+	double V0=cell.getVolume();
 
 	//Impose BC() en force: si une direction est controlee en vitesse, on impose hdd0 nulle dans cette dir
 	//Et on calcule le tenseur de contrainte internes a partir de l'equation ac terme de droite nul
@@ -535,41 +583,110 @@ void verletalgo_space(Cell& cell,std::vector<Particle>& ps, double dt){
 	//Si dir controle en vitesse, stressext dans cette dir = stress int
 	//On calcule les composantes de stressext dans cette direction
 	//Apres on fait le produit matriciel
+	//On recupere le stressint au debut du pas de temps (forces particules au debut du pas)
+	// computeInternalStress()
+	// Pour l'istant il est nul
+
+	//On applique les BCU pour recuperer les contraintes ext
 	cell.ApplyBC();
+	Tensor2x2 TotalStress = cell.getStressInt() +cell.getStressExt();
 
-	Tensor2x2 stressint = cell.getStressInt();
-	Tensor2x2 stressext = cell.getStressExt();
-
-	stressext.affiche();
-
-	double V0=cell.getVolume();
-	double mh=cell.getMasse();
 
 	//Calcul de hdd au debut du pas, hdd0
 	//Eq (24)
-	hdd0=hinv*(V0/mh)*(stressint+stressext);
+	//On a besoin de stressint au debut du pas de temps
+	hdd0=hinv*(V0/mh)*(TotalStress);
 
 	//Calcul position(h) avec hdd0 a la fin du pas de temps
 	h = h + hd*dt + hdd0*0.5*dt*dt;
+
+	//Calcul des positions particules fin du pas de temps
+
+	for(std::vector<Particle>::iterator it = ps.begin(); it != ps.end(); it++){
+
+		//Force pas de temps precedent:
+		Vecteur F = it->getForce(); 
+		Vecteur r = it->getR();
+		Vecteur v = it->getV();
+		double m = it->getMasse();
+
+		//Acceleration debut pas de temps
+		Vecteur tmp = hinv * r;
+		tmp = TotalStress * tmp; 
+		tmp = hinv * tmp;
+		tmp = tmp * (V0/mh);
+		//a0 a stocker
+		Vecteur a0 = F * (1./m) + tmp; 
+		r = r + v * dt + a0 * dt * dt * 0.5;
+		//update position et acceleration debut pas temps
+		it->setR(r,a0);
+	}
+
+	//On peut a present recalculer le tenseur des contraintes internes a partir des forces fin du pas de temps
+	//Calculer les forces a partir nouvelle position
+	//MD()
+	//computeInternalStress()
+	//Mettre a jout stressint de cell
+
 	//Volume fin du pas de temps
 	double V1=h.getDet();
 	//Calcul hdd fin du pas, hdd1
 	hinv=h.getInverse();
 
+	//Ensuite on calcule l'accleration de chaque particule fin du pas de temps en utilisant le nouveau h,V
+	//On a les vitesses des particules: position et vitesses particules fin pas de temps
+
+	cell.ApplyBC(); // permet de definir le stressext fin pas de temps
+	//TotalStress fin du pas de temps
+	TotalStress = cell.getStressInt() +cell.getStressExt(); 
+
+	TotalStress.affiche();
+	//Derniere boucle pour calcul des vitesses:
+	//Comment on applique les BCL en vitesse???
+	for(std::vector<Particle>::iterator it = ps.begin(); it != ps.end(); it++){
+
+		//Force recalculee fin du pas de temps:
+		Vecteur F = it->getForce(); 
+		//Position fin pas de temps
+		Vecteur r = it->getR();
+		//Acceleration debut pas de temps
+		Vecteur a0 = it->geta0();
+		double m = it->getMasse();
+
+		//Acceleration fin pas de temps
+		Vecteur tmp = hinv * r;
+		tmp = TotalStress * tmp; 
+		tmp = hinv * tmp;
+		tmp = tmp * (V1/mh);
+		Vecteur a1 = F * (1./m) + tmp; 
+		a1.print();
+		Vecteur v = it->getV() + (a1 + a0) * dt * 0.5 ;
+		v.print();
+		cout<<"vitesse:";
+		//update vitesse
+		it->setV(v);
+	}
+	//Fin integration particules
+
+
 	//Acceleration fin du pas: pareil que pour hdd0, on ompose hdd1 nul dans direction controle en vitesse
-	hdd1=hinv*(V1/mh)*(stressint+stressext);
+	//Il faut prendre le nouveau tenseur contrainte interne et externe en appliqand les BCU fin pas de temps
+
+	hdd1=hinv*(V1/mh)*(TotalStress);
 
 	//Calcul de hd fin du pas de temps
 	//Alors la vitesse ds dir controle en vitesse est bien constante (hdd0+hdd1=0)
 	hd=hd+(hdd0+hdd1)*dt*0.5;
 
-	//Impose BC(): on maintient les BC imposees en appliquant les coordonees de L_ imposee par utilisateur
-	//Du coup c'est redondant, je n'ai pas besoin de maintenir les BC en vitesse elles le seront automatiquement avec
-	//le traitement en force du dessus
-
 	//Update Cell:on fait remonter h,hd
 	cell.update(h,hd,dt);
-//	cell.affiche();
+}
+
+void write(std::vector<Particle> sp,ofstream& of,double t){
+	of<<t<<" ";
+	for(std::vector<Particle>::iterator it = sp.begin(); it != sp.end(); it++){
+		it->write(of);
+	}
 }
 
 int main(){
@@ -599,15 +716,14 @@ int main(){
 	do{
 		//Check for boundary:
 		cell.PeriodicBoundaries(p);
-		//Update position:
-		verletalgo_particles(cell,sample,dt);
-		verletalgo_space(cell,sample,dt);
+		//Update :
+		verletalgo(cell,sample,dt);
 
 		//outputs:
-		if( k% 10 == 0 ){
+		if( k% 1 == 0 ){
 			cell.write(tmp2,tmp3,t);
-			p.write(tmp);
-			cout<<t<<" "<<cell.getVolume()<<endl;
+			write(sample,tmp,t);
+			//cout<<t<<" "<<cell.getVolume()<<endl;
 		}
 
 		t+=dt;
